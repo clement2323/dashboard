ee8514a124e53678984bcc9724735285e7a4b04d
clement <guillo.clement@gmail.com>
2023-12-23 08:39:51 -0400

    Fin de la TopBar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { ColorModeContext, useMode } from "./themes";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
 function App() {
+  const [theme,colorMode] = useMode();
   return (
+    <ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+    <CssBaseline />
     <div className="App">
-      
+      <main className ="content"> 
+      <Topbar />
+      </main>
     </div>
+    </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 

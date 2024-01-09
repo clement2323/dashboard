ab3a110c6ea33d39f0022d9946c993f8ce080c15
clement <guillo.clement@gmail.com>
2024-01-09 01:47:32 +0100

    tooltip pour modif couleur des labels sur camembert

--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -31,26 +31,13 @@ const PieChart = () => {
         from: "color",
         modifiers: [["darker", 2]],
       }}
-      defs={[
-        {
-          id: "dots",
-          type: "patternDots",
-          background: "inherit",
-          color: "rgba(255, 255, 255, 0.3)",
-          size: 4,
-          padding: 1,
-          stagger: true,
+      theme={{
+        tooltip: {
+          container: {
+            color: `${colors.grey[500]}`,
+          },
         },
-        {
-          id: "lines",
-          type: "patternLines",
-          background: "inherit",
-          color: "rgba(255, 255, 255, 0.3)",
-          rotation: -45,
-          lineWidth: 6,
-          spacing: 10,
-        },
-      ]}
+      }}
       legends={[
         {
           anchor: "bottom",
@@ -61,7 +48,7 @@ const PieChart = () => {
           itemsSpacing: 0,
           itemWidth: 100,
           itemHeight: 18,
-          itemTextColor: "#999",
+          itemTextColor: "",
           itemDirection: "left-to-right",
           itemOpacity: 1,
           symbolSize: 18,

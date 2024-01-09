bf080a01726b476cf1155b24fbbc367b5c0b243b
clement <guillo.clement@gmail.com>
2024-01-09 11:53:58 +0100

    bug liunechart corrige

--- src/scenes/line/index.jsx
+++ src/scenes/line/index.jsx
@@ -1,13 +1,14 @@
 import { Box } from "@mui/material";
 import Header from "../../components/Header";
 import LineChart from "../../components/LineChart";
+import {dataBarEEC} from "../../data/DuckDbData.js";
 
 const Line = () => {
   return (
     <Box m="20px">
       <Header title="Enquête EEC" subtitle="taux de collecte au cumulé par semaine" />
       <Box height="75vh">
-        <LineChart />
+        <LineChart data ={dataBarEEC} />
       </Box>
     </Box>
   );
